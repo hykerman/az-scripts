bea98093807daeae9c7df27050cd838e77c5813c
hykerman <gyaeger90@hotmail.com>
2021-11-29 13:16:53 -0600

    fix package list

--- install_packages.sh
+++ install_packages.sh
@@ -5,14 +5,7 @@
 #-------------------------------------------------------------------------------------------------------
 
 sudo apt-get update
-sudo apt-get install -y \ 
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common \
-    gnupg-agent \
-    gnupg2 \
-    pass
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg-agent gnupg2 pass
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
